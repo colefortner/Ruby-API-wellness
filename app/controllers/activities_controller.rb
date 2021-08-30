d26230e104ef1d81734edbb0601764e0b4d9403a
Cole Fortner <colefortner@gmail.com>
2021-08-29 23:27:13 -0400

    Add index and create methods to activities_controller.rb

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,28 @@
 class ActivitiesController < ApplicationController
+  def index
+    daily_activity_graph = Activites.select(:steps, :date, :miles, :cardio_minutes)
+    # daily_datum = DailyDatum.all 
+    render json: {
+      daily_activity_graph: daily_activity_graph
+    }
+  end
+  def create
+    daily_activty = DailyDatum.create!(
+      date: params['activity']['date'],
+      steps: params['activity']['steps'],
+      cardio_minutes: params['activity']['cardio_minutes'],
+      stairs: params['activity']['stairs'],
+      miles: params['activity']['miles'],
+      calories: params['activity']['calories'],
+    )
+
+    if daily_data
+      render json: {
+        status: :created,
+        daily_activity: daily_activity 
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
 end
